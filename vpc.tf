eac0fc8a179c1e5452e9b53380db3b42dda94d07
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-03-05 00:53:32 +0530

    working on vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/DevSecOps-ssr/Terraform-aws-vpc.git?ref=main"
-  project = "Roboshop"
-  environment = "dev"
+  project = var.project
+  environment = var.environment
 
   
-}
+} 
