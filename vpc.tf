9193ee5c26378fa35cdbff39249e0c6547d1c57d
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-03-05 13:57:30 +0530

    working on vpc peering

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "git::https://github.com/DevSecOps-ssr/Terraform-aws-vpc.git?ref=main"
   project = var.project
   environment = var.environment
+  is_peering_required = true
 
   
 } 
